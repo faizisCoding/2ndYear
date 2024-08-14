a96e007a331dc17720ca9224945b966214de5e28
faizisCoding <faiz.gm12@gmail.com>
2024-08-14 20:00:25 +0530

    added q4 from to Week4

---
+++ OOP/Week4/Counter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+//import java.io.*;
+class Counter{
+    private static int count=0;
+Counter(){ count++; } 
+    static void showCount(){ //method which shows no. of objects created
+        System.out.println("No. of objects created = "+count);
+    }
+    public static void main(String[] args){
+        System.out.print("Enter number of objects: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=0; i < n ; i++) {Counter c = new Counter();} //creating n objects
+        Counter.showCount(); //displaying no. of objects created 
+    }
+}
